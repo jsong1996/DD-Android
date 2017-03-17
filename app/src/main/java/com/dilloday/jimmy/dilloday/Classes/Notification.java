9daffdb18ce45d251d98eec02df4d457dac48294
jsong1996 <jimmysong2018@u.northwestern.edu>
2017-03-17 15:17:46 -0500

    Notifications Finished, UI Changed

---
+++ app/src/main/java/com/dilloday/jimmy/dilloday/Classes/Notification.java
@@ -0,0 +1,17 @@
+package com.dilloday.jimmy.dilloday.Classes;
+
+/**
+ * Created by Jimmy on 3/17/2017.
+ */
+public class Notification {
+
+    public String MessageBody;
+
+    public Notification() {
+    }
+
+    public Notification( String MessageBody)
+    {
+        this.MessageBody = MessageBody;
+    }
+}
--- app/src/main/java/com/dilloday/jimmy/dilloday/HomeTab/Tab3Activity.java
+++ app/src/main/java/com/dilloday/jimmy/dilloday/HomeTab/Tab3Activity.java
@@ -10,7 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.dilloday.jimmy.dilloday.Classes.Notification;
+import com.dilloday.jimmy.dilloday.Classes.dbArtist;
 import com.dilloday.jimmy.dilloday.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +32,46 @@ public class Tab3Activity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
-//
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.fragment_notifications, R.id.notifications_list_textview, alert);
-//        ListView listview = (ListView) rootView.findViewById(R.id.alert_listView);
-//        listview.setAdapter(adapter);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        final DatabaseReference notificationsRef= database.getReference("Notifications/");
+        notificationsRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+                String notification = dataSnapshot.getValue(String.class);
+                System.out.println("Notification Body: " + notification);
+                if (!alert.contains(notification)){
+                    //System.out.println("Reaches Here");
+                    alert.add(notification);
+                }
+                //System.out.println(alert);
+                }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+                Notification n = dataSnapshot.getValue(Notification.class);
+                //System.out.println("The updated post title is: " + changedPost.Name);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String notification = dataSnapshot.getValue(String.class);
+                if (alert.contains(notification)){
+                    alert.remove(notification);
+                }
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+        //System.out.println(alert);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.fragment_notifications, R.id.txt_notification, alert);
+        ListView listview = (ListView) rootView.findViewById(R.id.alert_listView);
+        listview.setAdapter(adapter);
 
         return rootView;
     }
